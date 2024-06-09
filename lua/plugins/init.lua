debb848766f6cbc29abd02dc8aabb45850b7a2d9
Dennis Damen <dennis.damen@gmail.com>
2024-06-09 08:58:47 +0200

    First commmit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require "configs.none-ls"
+    end,
+  },
+  {
+     "neovim/nvim-lspconfig",
+     config = function()
+       require("nvchad.configs.lspconfig").defaults()
+       require "configs.lspconfig"
+     end,
+  },
+  {
+    "williamboman/mason.nvim",
+   	opts = {
+   	  ensure_installed = {
+   		  "lua-language-server", "stylua",
+   			"html-lsp", "css-lsp" , "prettier",
+        "clangd", "clang-format"
+   		},
+   	},
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+   	opts = {
+   		ensure_installed = {
+   			"vim", "lua", "vimdoc",
+        "html", "css", "cpp", "bash"
+   		},
+   	},
+   },
+}
